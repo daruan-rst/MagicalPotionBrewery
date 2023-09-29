d9312dc94a20cf99df3fa89d2f6153067b00b9e7
daruan-rst <daruan.rst@gmail.com>
2023-09-28 22:48:47 -0300

    Adding @Repository annotation

--- src/main/kotlin/magic/potion/shop/repositories/PotionRepository.kt
+++ src/main/kotlin/magic/potion/shop/repositories/PotionRepository.kt
@@ -2,6 +2,8 @@ package magic.potion.shop.repositories
 
 import magic.potion.shop.model.Potion
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface PotionRepository : JpaRepository<Potion, Long?> {
 }
--- src/main/kotlin/magic/potion/shop/repositories/WizardRepository.kt
+++ src/main/kotlin/magic/potion/shop/repositories/WizardRepository.kt
@@ -2,6 +2,8 @@ package magic.potion.shop.repositories
 
 import magic.potion.shop.model.Wizard
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface WizardRepository : JpaRepository<Wizard, Long?> {
 }
