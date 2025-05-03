5b2da8b4213588dd58019b31f94d7dff5e678df4
Daruan <daruan.rst@gmail.com>
2025-05-03 17:12:33 -0300

    Ingredient Service Tests

--- src/test/kotlin/magic/potion/shop/service/IngredientServiceTest.kt
+++ src/test/kotlin/magic/potion/shop/service/IngredientServiceTest.kt
@@ -1,10 +1,30 @@
 package magic.potion.shop.service
 
+import junit.framework.TestCase.assertEquals
+import magic.potion.shop.model.Ingredient
+import magic.potion.shop.model.IngredientFlavor
+import magic.potion.shop.repositories.IngredientRepository
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-
-import org.junit.jupiter.api.Assertions.*
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito.any
+import org.mockito.Mockito.`when`
+import org.mockito.MockitoAnnotations
 
 class IngredientServiceTest {
+
+
+   @Mock
+   private lateinit var ingredientRepository: IngredientRepository
+
+   @InjectMocks
+   private lateinit var ingredientService: IngredientService
+
+   @BeforeEach
+   fun setUp(){
+      MockitoAnnotations.openMocks(this);
+   }
 
     @Test
     fun findAll() {
@@ -20,6 +40,15 @@ class IngredientServiceTest {
 
     @Test
     fun createIngredient() {
+       var testIngredient = Ingredient(0, "Dance Apple", IngredientFlavor.SWEET)
+
+       `when`(ingredientRepository.save(any<Ingredient>())).thenReturn(testIngredient)
+
+       var ingredient =  ingredientService.createIngredient(testIngredient)
+
+       assertEquals(ingredient.id, testIngredient.id)
+       assertEquals(ingredient.name, testIngredient.name)
+       assertEquals(ingredient.flavor, testIngredient.flavor)
     }
 
     @Test
@@ -29,4 +58,6 @@ class IngredientServiceTest {
     @Test
     fun delete() {
     }
+
+
 }
