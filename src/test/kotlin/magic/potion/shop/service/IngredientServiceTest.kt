3517cbec9a964ec1a19669edb85fbff41b0a1dde
Daruan <daruan.rst@gmail.com>
2025-05-03 17:12:33 -0300

    Ingredient Service Tests

---
+++ src/test/kotlin/magic/potion/shop/service/IngredientServiceTest.kt
@@ -0,0 +1,32 @@
+package magic.potion.shop.service
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class IngredientServiceTest {
+
+    @Test
+    fun findAll() {
+    }
+
+    @Test
+    fun findById() {
+    }
+
+    @Test
+    fun findIngredientByName() {
+    }
+
+    @Test
+    fun createIngredient() {
+    }
+
+    @Test
+    fun updateIngredient() {
+    }
+
+    @Test
+    fun delete() {
+    }
+}
